13c7ddeb7ba5af494f40584340b83b9c9ca1ec5d
Avdi Grimm <avdi@avdi.codes>
2023-01-09 19:29:09 +0000

    Just enough to get a rodauth login to display

--- Gemfile
+++ Gemfile
@@ -9,4 +9,14 @@ group :development do
   gem "solargraph", "~> 0.48.0"
   gem "rackup", "~> 0.2.3"
   gem "rerun", "~> 0.14.0"
+  gem "sqlite3", "~> 1.5"
 end
+
+gem "rodauth", "~> 2.26"
+
+gem "rodauth-oauth", "~> 1.0"
+
+gem "bcrypt", "~> 3.1"
+
+
+gem "jwt", "~> 2.6"
--- lib/frobozz/app.rb
+++ lib/frobozz/app.rb
@@ -1,8 +1,49 @@
+require "sequel"
 require "roda"
+require "rodauth"
+require "rodauth/oauth"
+require "jwt"
+
+case ENV["ENVIRONMENT"].downcase
+when "development"
+  DB = Sequel.sqlite
+else
+  raise "An ENVIRONMENT is required"
+end
+
+DB.create_table :account_statuses do
+  Integer :id, primary_key: true
+  String :name, null: false, unique: true
+end
+DB.from(:account_statuses).import([:id, :name], [[1, "Unverified"], [2, "Verified"], [3, "Closed"]])
+DB.create_table :accounts do
+  uuid :id, primary_key: true, default: Sequel.function(:gen_random_uuid)
+  foreign_key :status_id, :account_statuses, null: false, default: 1
+  if DB.database_type == :postgresql
+    citext :email, null: false
+    constraint :valid_email, email: /^[^,;@ \r\n]+@[^,@; \r\n]+\.[^,@; \r\n]+$/
+  else
+    String :email, null: false
+  end
+  if DB.supports_partial_indexes?
+    index :email, unique: true, where: {status_id: [1, 2]}
+  else
+    index :email, unique: true
+  end
+
+  String :ph
+end
 
 module Frobozz
   class App < Roda
+    plugin :rodauth, json: true do
+      db DB
+      enable :login, :oauth_authorization_code_grant, :jwt
+      oauth_application_scopes %w[profile.read profile.write]
+    end
+    plugin :sessions, secret: "TODO_BETTER_SECRET" * 4
     route do |r|
+      r.rodauth
       r.on "hello" do
         "Hello, world. The time is now #{Time.now}"
       end
