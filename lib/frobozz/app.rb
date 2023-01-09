bdea85bbb2778811b5de974693ffb4f48e45b812
Avdi Grimm <avdi@avdi.codes>
2023-01-09 22:26:32 +0000

    Add a test user

--- lib/frobozz/app.rb
+++ lib/frobozz/app.rb
@@ -8,7 +8,7 @@ case ENV["ENVIRONMENT"].downcase
 when "development"
   DB = Sequel.sqlite
 else
-  raise "An ENVIRONMENT is required"
+  raise "An ENVIRONMENT of #{ENV["ENVIRONMENT"]} is not [yet] supported"
 end
 
 DB.create_table :account_statuses do
@@ -17,7 +17,7 @@ DB.create_table :account_statuses do
 end
 DB.from(:account_statuses).import([:id, :name], [[1, "Unverified"], [2, "Verified"], [3, "Closed"]])
 DB.create_table :accounts do
-  uuid :id, primary_key: true, default: Sequel.function(:gen_random_uuid)
+  primary_key :id, type: :Bignum
   foreign_key :status_id, :account_statuses, null: false, default: 1
   if DB.database_type == :postgresql
     citext :email, null: false
@@ -31,14 +31,17 @@ DB.create_table :accounts do
     index :email, unique: true
   end
 
-  String :ph
+  String :password_hash
 end
+very_secure_password = BCrypt::Password.create("poop", cost: BCrypt::Engine::MIN_COST).to_s
+DB[:accounts].insert email: "avdi@avdi.codes", status_id: 2, password_hash: very_secure_password
 
 module Frobozz
   class App < Roda
     plugin :rodauth, json: true do
       db DB
-      enable :login, :oauth_authorization_code_grant, :jwt
+      account_password_hash_column :password_hash
+      enable :login, :logout, :oauth_authorization_code_grant, :jwt
       oauth_application_scopes %w[profile.read profile.write]
     end
     plugin :sessions, secret: "TODO_BETTER_SECRET" * 4
